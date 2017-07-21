aef6ab2c9d0a6f146cb11b4a8cbd6c5d38823c9c
PoC-Yeh <allisonxp7@gmail.com>
2017-07-21 15:57:08 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pickle
+import jieba
+import progressbar
+import csv
+from jieba_cut import *
+
+jieba_cut("Japan_Travel", "textpickle")
+#/home/ptt/ptt_Japan_Travel_textpickle.txt
+#first arg -> board name
+#second arg -> "textpickle" or "text_pickle"
